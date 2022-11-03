29605519ad001d1a4a5f3dd8cb54eba3ffb181c2
Shubh0405 <shubhnaresh.gupta2019@vitstudent.ac.in>
2022-11-04 00:53:37 +0530

    theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
 import 'package:flutter_application_1/utils/routes.dart';
+import 'package:flutter_application_1/widgets/themes.dart';
 import 'pages/home_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,10 +16,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         // home: HomePage(),
         themeMode: ThemeMode.light,
-        theme: ThemeData(
-            primarySwatch: Colors.deepPurple,
-            fontFamily: GoogleFonts.lato().fontFamily),
-        darkTheme: ThemeData(brightness: Brightness.dark),
+        theme: MyThemes.lightTheme(context),
+        darkTheme: MyThemes.darkTheme(context),
         debugShowCheckedModeBanner: false,
         initialRoute: MyRoutes.homeRoute,
         routes: {
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,11 +9,11 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        centerTitle: true,
+        title: const Text("Catalog App"),
       ),
       body: Center(
-        child: Container(
-            child: Text("Hello! Welcome to $app_name ! token no: $days")),
+        child: Text("Hello! Welcome to $app_name ! token no: $days"),
       ),
       drawer: MyDrawer(),
     );
