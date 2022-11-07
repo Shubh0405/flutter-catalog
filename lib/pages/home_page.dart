fc3abeb29f9526270a1673e7fbf66dec1f821330
Shubh0405 <shubhnaresh.gupta2019@vitstudent.ac.in>
2022-11-08 03:05:26 +0530

    data loading and class generator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+import 'dart:convert';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   num days = 20;
+
   String app_name = "FoodMall";
 
   @override
-  Widget build(BuildContext context) {
-    final dummylist = List.generate(50, (index) => CatalogModel.items[0]);
+  void initState() {
+    super.initState();
+    loadData();
+  }
 
+  loadData() async {
+    // await Future.delayed(Duration(seconds: 5));
+    final catalogjson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogjson);
+    var productData = decodedData["products"];
+    print(productData);
+    CatalogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -19,18 +43,22 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         color: Colors.white,
-        child: ListView.builder(
-          itemCount: dummylist.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 5.0, horizontal: 12.0),
-              child: ItemWidget(
-                item: dummylist[index],
+        child: (CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 5.0, horizontal: 12.0),
+                    child: ItemWidget(
+                      item: CatalogModel.items[index],
+                    ),
+                  );
+                },
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
               ),
-            );
-          },
-        ),
       ),
       drawer: MyDrawer(),
     );
